769bfbbb31885d3930e43e1a40bcf93d211e9564
Amadeusz Wieczorek <git@amadeusw.com>
2017-05-12 17:17:36 -0700

    Transmitter

--- src/Analyzer.Library/Tracer.cs
+++ src/Analyzer.Library/Tracer.cs
@@ -8,16 +8,19 @@ namespace Analyzer.Library
         public void Post(object value)
         {
             var data = new LoggedData(value);
+            Transmitter.Post(data);
         }
 
         public void Post(object value, string name = "")
         {
             var data = new LoggedData(value, name);
+            Transmitter.Post(data);
         }
 
         public void Post(object value, string name = "", [CallerMemberName] string caller = "", [CallerFilePath] string file = "", [CallerLineNumber] int lineNumber = 0)
         {
             var data = new LoggedData(value, name, caller, file, lineNumber);
+            Transmitter.Post(data);
         }
     }
 }
---
+++ src/Analyzer.Library/Transmitter.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Analyzer.Library
+{
+    internal class Transmitter
+    {
+        internal static void Post(LoggedData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
