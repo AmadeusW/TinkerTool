c5801072ae6239378b50485a3ff0e16c4a6b8d1d
Amadeusz Wieczorek <git@amadeusw.com>
2017-05-12 17:20:55 -0700

    Transmitter

--- src/Analyzer.Library/Transmitter.cs
+++ src/Analyzer.Library/Transmitter.cs
@@ -4,6 +4,23 @@ namespace Analyzer.Library
 {
     internal class Transmitter
     {
+        static Transmitter _instance;
+        static Transmitter Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+                else
+                {
+                    _instance = new Transmitter();
+                    return _instance;
+                }
+            }
+        }
+
         internal static void Post(LoggedData data)
         {
             throw new NotImplementedException();
