3fdb11cbecb1bc3e4a710e70f2bde8be3fa22619
Amadeusz Wieczorek <amwieczo@microsoft.com>
2017-06-09 09:10:04 -0700

    basic assignment parser

--- src/Analyzer.App/Hubs/Tracer.cs
+++ src/Analyzer.App/Hubs/Tracer.cs
@@ -7,6 +7,16 @@ namespace Analyzer.App.Hubs
     {
         public Task Send(string data)
         {
+            if (data.Contains("="))
+            {
+                var parsed = data.Split("=");
+                if (parsed.Length == 2 && !string.IsNullOrWhiteSpace(parsed[0]))
+                {
+                    var property = parsed[0].Trim();
+                    var value = parsed[1].Trim();
+                    return Clients.All.InvokeAsync("Send", $"[{property}] := [{value}]");
+                }
+            }
             return Clients.All.InvokeAsync("Send", data);
         }
     }
