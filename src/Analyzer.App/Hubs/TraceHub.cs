49c88e5132cfbcace872fa78523339d87bf4376e
Amadeusz Wieczorek <git@amadeusw.com>
2018-12-05 21:38:26 -0800

    updates to .net core 2.2, signalr2, follows tutorial

---
+++ src/Analyzer.App/Hubs/TraceHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analyzer.App.Hubs
+{
+    public class TraceHub : Hub
+    {
+        public TraceHub()
+        {
+
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("TraceHub.SendMessage", user, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+    }
+}
